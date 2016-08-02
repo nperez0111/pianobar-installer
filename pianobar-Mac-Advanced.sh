994b4dd984ee040c876796d0fc852dc80175482c
Nick The Sick <nperez0111@gmail.com>
2016-08-01 23:08:32 -0400

    fix escapes

--- pianobar-Mac-Advanced.sh
+++ pianobar-Mac-Advanced.sh
@@ -125,15 +125,15 @@ if trigger == 'songstart'
 
   STDIN.each_line { |line| songinfo.store(*line.chomp.split('=', 2))}
 
-  echo \`terminal-notifier -title \"Pianobar\" -subtitle \"#{songinfo['title']} by #{songinfo['artist']}\" -group \"Pianobar\" ${image} ${showTerm} -message \"album: '#{songinfo['album']}' on #{songinfo['stationName']}\" -contentImage \"#{songinfo['coverArt']}\"\`
+  \`terminal-notifier -title "Pianobar" -subtitle "#{songinfo['title']} by #{songinfo['artist']}" -group "Pianobar" ${image} ${showTerm} -message "album: '#{songinfo['album']}' on #{songinfo['stationName']}" -contentImage "#{songinfo['coverArt']}"\`
 
 elsif trigger == 'userlogin'
 
-	echo \`terminal-notifier -title \"Pianobar Started\" -message \"Welcome back\" -group \"Pianobar\" ${image}\`
+	\`terminal-notifier -title "Pianobar Started" -message "Welcome back" -group "Pianobar" ${image}\`
 
 elsif trigger == 'stationfetchplaylist'
 
-	echo \`terminal-notifier -title \"Fetching songs...\" -message \"Changing stations\" -group \"Pianobar\" ${image}\`
+	\`terminal-notifier -title "Fetching songs..." -message "Changing stations" -group "Pianobar" ${image}\`
 
 end
 EOT
