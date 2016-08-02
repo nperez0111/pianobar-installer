7c86384ce69f996789955714850f969570f7d49b
Nick The Sick <nperez0111@gmail.com>
2016-08-01 22:45:27 -0400

    will an echo fix this

--- pianobar-Mac-Advanced.sh
+++ pianobar-Mac-Advanced.sh
@@ -125,15 +125,15 @@ if trigger == 'songstart'
 
   STDIN.each_line { |line| songinfo.store(*line.chomp.split('=', 2))}
 
-  `terminal-notifier -title "Pianobar" -subtitle "#{songinfo['title']} by #{songinfo['artist']}" -group "Pianobar" ${image} ${showTerm} -message "album: '#{songinfo['album']}' on #{songinfo['stationName']}" -contentImage "#{songinfo['coverArt']}"`
+  `echo terminal-notifier -title "Pianobar" -subtitle "#{songinfo['title']} by #{songinfo['artist']}" -group "Pianobar" ${image} ${showTerm} -message "album: '#{songinfo['album']}' on #{songinfo['stationName']}" -contentImage "#{songinfo['coverArt']}"`
 
 elsif trigger == 'userlogin'
 
-	`terminal-notifier -title "Pianobar Started" -message "Welcome back" -group "Pianobar" ${image}`
+	`echo terminal-notifier -title "Pianobar Started" -message "Welcome back" -group "Pianobar" ${image}`
 
 elsif trigger == 'stationfetchplaylist'
 
-	`terminal-notifier -title "Fetching songs..." -message "Changing stations" -group "Pianobar" ${image}`
+	`echo terminal-notifier -title "Fetching songs..." -message "Changing stations" -group "Pianobar" ${image}`
 
 end
 EOT
