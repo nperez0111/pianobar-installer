fa9d73cbbda2e7f4131641e3b1a2ed1a414b3242
Nick The Sick <nperez0111@gmail.com>
2016-08-02 21:10:45 -0400

    made pianobarNotify a little more readable

--- pianobarNotify.rb
+++ pianobarNotify.rb
@@ -1,14 +1,16 @@
 #!/usr/bin/ruby
 
 trigger = ARGV.shift
-
+appIcon = "~/.config/pianobar/PandoraIco.png"
+defaultTerminal = "com.apple.terminal"
+#com.googlecode.iterm2 for iterm2
 songinfo = {}
 STDIN.each_line { |line| songinfo.store(*line.chomp.split('=', 2))}
 
 if trigger == 'songstart'
-	`terminal-notifier -title "#{songinfo['title']}" -subtitle "By:#{songinfo['artist']}" -group "Pianobar" -appIcon "~/.config/pianobar/PandoraIco.png" -activate "com.googlecode.iterm2" -message "Album:#{songinfo['album']} on #{songinfo['stationName']}" -contentImage "#{songinfo['coverArt']}"`
+	`terminal-notifier -title "#{songinfo['title']}" -subtitle "By:#{songinfo['artist']}" -group "Pianobar" -appIcon "#{appIcon}" -activate "#{defaultTerminal}" -message "Album:#{songinfo['album']} on #{songinfo['stationName']}" -contentImage "#{songinfo['coverArt']}"`
 elsif trigger == 'userlogin'
-	`terminal-notifier -title "Pianobar Started" -message "Welcome back" -group "Pianobar" -appIcon "~/.config/pianobar/PandoraIco.png"`
+	`terminal-notifier -title "Pianobar Started" -message "Welcome back" -group "Pianobar" -appIcon "#{appIcon}"`
 elsif trigger == 'stationfetchplaylist'
-	`terminal-notifier -title "Fetching songs..." -message "Changing stations" -group "Pianobar" -appIcon "~/.config/pianobar/PandoraIco.png"`
+	`terminal-notifier -title "Fetching songs..." -message "Changing stations" -group "Pianobar" -appIcon "#{appIcon}"`
 end
