ddd109a726f08d4e5eca1a1b18c74d745c69b7b7
Nick The Sick <nperez0111@gmail.com>
2016-08-03 00:35:02 -0400

    updated colors

--- pianobar-Mac-Simple.sh
+++ pianobar-Mac-Simple.sh
@@ -137,11 +137,11 @@ cat <<EOT >> config
 user = ${username}
 password = ${password}
 autostart_station = ${autostart}
-format_nowplaying_song = "[92m%t[0m", by: "[96m%a[0m" on the album: "[93m%l[0m"[91m%r[0m%@%s
-format_nowplaying_station = Station "[95m%n[0m" [90m(%i)[0m
+format_nowplaying_song = [93m%t[0m, by: [93m%a[0m on the album: [96m%l[0m [91m%r[0m%@%s
+format_nowplaying_station = Now Playing "[95m%n[0m" [90m(%i)[0m
 format_list_song = %i) %a - [92m%t[0m%r
 format_msg_info = [97m(i) [0m%s
-format_msg_nowplaying = [36m|>[0m  %s
+format_msg_nowplaying = [36m>[0m  %s
 format_msg_time = [90m#   [97m%s[0m
 format_msg_err = [90m/!\[0m %s
 format_msg_question = [97m[?][0m %s
